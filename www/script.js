a413c88e365bd15a73472130a8de0ad130d55484
VictorGurgell <slakaguradc@gmail.com>
2024-05-29 08:48:09 +0900

    Project's Initial Commit

---
+++ www/script.js
@@ -0,0 +1,23 @@
+function din() {
+    window.location.href = "dinheiro.html";
+}
+
+function rou() {
+    window.location.href = "roupa.html";
+}
+
+function com() {
+    window.location.href = "comida.html";
+}
+
+function pag() {
+    window.location.href = "pagamento.html";
+}
+
+function avi() {
+    window.location.href = "aviso.html";
+}
+
+function vol() {
+    window.location.href = "index.html";
+}
